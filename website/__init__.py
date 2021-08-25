49df0630ad7a2a0038781030e37741545869a0d9
OmarHoussami <omar.houssami20@gmail.com>
2021-08-25 18:18:08 +0300

    importing the blueprints to the init file

--- website/__init__.py
+++ website/__init__.py
@@ -3,4 +3,8 @@ from flask import Flask
 def create_app():
   app = Flask(__name__)
   app.config['SECRET_KEY'] = 'qwertyuiopasdfghjklzxcvbnm'
+  
+  from .views import views
+  from .auth import auth
+  
   return app
